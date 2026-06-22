9bfdd88fc3f643c38de8b0ee91f2cb01d66188ea
gnailegoac <gnailegoac@users.noreply.github.com>
2026-06-22 14:20:43 +0000

    Configure Supabase project

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.WORLD_CUP_LOOKUP_SUPABASE = {
-  url: "",
-  anonKey: "",
+  url: "https://bzcynwyopkhvqlwrrqrk.supabase.co",
+  anonKey: "sb_publishable_7LdT3jbNmWK7FWiM90E5ZA_oaIiseuL",
 };
